d1ae7f94cb4cce0b1d98498513e578a2422222d7
m.shchepalin <m.shchepalin@tinkoff.ru>
2022-05-06 23:39:58 +0300

    added poll bazaraki

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import logging
+
 from di import Di
 
 TOKEN = "5168767958:AAEk_XoqEaS9S8bxy9qfBeFiAIg5Xi20okE"
 
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 def main():
     Di(TOKEN, 30).get_bot().start()
