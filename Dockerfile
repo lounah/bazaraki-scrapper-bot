2d1e365908c2a3d972ad2c4c53e900b3c5d193a9
m.shchepalin <m.shchepalin@tinkoff.ru>
2022-05-12 14:19:41 +0300

    Added __init__.py

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,6 @@ FROM python:3.9.6
 
 LABEL maintainer="<lounah@yandex.ru>"
 
-ENV TZ "Europe/Moscow"
-ENV LANG en_US.UTF-8
-
 WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
--- src/app.py
+++ src/app.py
@@ -1,12 +1,13 @@
 import os
+import sys
 
 from di.di import Di
 
 
-def main():
+def main(argv):
     di = Di(os.getenv('token'), os.getenv('port'), os.getenv('url'), os.getenv('cert'), os.getenv('key'))
     di.bot().start()
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
